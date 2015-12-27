92d246914f33da146836c5c7aa6ce47a6e4bbe2b
Sota Yamashita <sota.yamashita@gmail.com>
2015-12-28 00:27:43 +0900

    Add: libs

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: [
     require("babel-preset-react")
+  ],
+  plugins: [
+    require("babel-plugin-transform-es2015-literals"),
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-shorthand-properties"),
+    require("babel-plugin-transform-decorators"),
+    require("babel-plugin-transform-object-rest-spread")
   ]
-}
+};

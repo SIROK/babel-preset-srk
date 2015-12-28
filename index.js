5e7dbb953412656d33f679ea0c35bd0abd1912f0
Sota Yamashita <sota.yamashita@gmail.com>
2015-12-28 11:22:19 +0900

    Remove babel-plugin-transform-decorators

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
     require("babel-plugin-transform-es2015-literals"),
     require("babel-plugin-transform-es2015-arrow-functions"),
     require("babel-plugin-transform-es2015-shorthand-properties"),
-    require("babel-plugin-transform-decorators"),
     require("babel-plugin-transform-object-rest-spread")
   ]
 };

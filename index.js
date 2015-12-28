8183f778b6cdee575e6c672cc2ab1f47631091ab
Sota Yamashita <sota.yamashita@gmail.com>
2015-12-28 12:30:42 +0900

    Add: babel-plugin-transform-es2015-computed-properties

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     require("babel-plugin-transform-es2015-literals"),
     require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-computed-properties"),
     require("babel-plugin-transform-es2015-shorthand-properties"),
     require("babel-plugin-transform-object-rest-spread")
   ]

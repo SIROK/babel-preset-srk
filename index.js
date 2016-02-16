1d3dd9bf5c75c6e412b9a4f20b7c82afda9bb6c7
Sota Yamashita <sota.yamashita@gmail.com>
2016-02-16 17:46:22 +0900

    Replace: babel-preset-stage-0 to specific plugins

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = {
   presets: [
     require("babel-preset-es2015"),
-    require("babel-preset-stage-0"),
     require("babel-preset-react")
+  ],
+  plugins: [
+    require("babel-plugin-transform-class-properties")
   ]
 };
